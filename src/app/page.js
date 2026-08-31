d07436e7feb55de7e68f9c5a59d9f671c67f7218
Rotak0411 <Rotrma74@osu.cz>
2026-08-31 17:38:24 +0200

    sluzby a onas a poptat

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { Suspense } from 'react';
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import {
